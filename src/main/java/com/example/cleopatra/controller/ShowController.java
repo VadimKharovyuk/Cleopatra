2a4e438bea86b86fd17c3a16d16e369701a2ff0e
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-06-15 13:56:10 +0300

    update MainPage

---
+++ src/main/java/com/example/cleopatra/controller/ShowController.java
@@ -0,0 +1,15 @@
+package com.example.cleopatra.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/show")
+public class ShowController {
+
+    @GetMapping
+    public String show() {
+        return "show";
+    }
+}
