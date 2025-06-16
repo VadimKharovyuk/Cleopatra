04cce8aba96d18480d8da1cd9054b3e6108212ff
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-06-16 15:39:06 +0300

    update sidebar.html

--- src/main/java/com/example/cleopatra/repository/PostRepository.java
+++ src/main/java/com/example/cleopatra/repository/PostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -80,4 +81,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     long countByCreatedAtBetween(LocalDateTime startOfDay, LocalDateTime endOfDay);
 
     long countByCreatedAtGreaterThanEqual(LocalDateTime startDate);
+
+
+     @Query("UPDATE Post p SET p.viewsCount = p.viewsCount + 1 WHERE p.id = :id")
+ @Modifying
+ int incrementViewsCount(@Param("id") Long id);
 }
